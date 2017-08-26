e99ee1accb23ab8add44a1b87c519a91775c84eb
Drew Keller <drew@eightycolumns.com>
2017-08-25 21:14:01 -0400

    Emit and handle an event

---
+++ event_emitters.js
@@ -0,0 +1,19 @@
+const http = require('http');
+
+const EventEmitter = require('events');
+
+const server = http.createServer((request, response) => {
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+
+  const eventEmitter = new EventEmitter();
+
+  eventEmitter.on('foo', () => {
+    response.write('Handling event "foo".\n');
+  });
+
+  eventEmitter.emit('foo');
+
+  response.end();
+});
+
+server.listen(3000);
